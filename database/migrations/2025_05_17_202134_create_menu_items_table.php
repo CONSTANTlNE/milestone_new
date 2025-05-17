b4165902daa14359f654d911024f3cb941fed484
Boris Barabadze <boris.barabadze@flatrocktech.com>
2025-05-18 00:37:03 +0400

    igration make site menus and menu items

---
+++ database/migrations/2025_05_17_202134_create_menu_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained()->cascadeOnDelete();
+            $table->json('title');
+            $table->json('slug');
+            $table->integer('status')->default(1)->index();
+            $table->string('route')->nullable();
+            $table->integer('is_prefix')->default(1)->index();
+            $table->unsignedBigInteger('parent_id')->nullable()->index();
+            $table->integer('sort')->default(0)->index();
+            $table->integer('depth')->default(0)->index();
+            $table->string('class', 100)->nullable();
+            $table->string('icon_class')->nullable();
+            $table->unsignedBigInteger('model_id')->nullable()->index();
+            $table->string('fileable_type', 100)->nullable();
+            $table->string('target')->default('_self')->index();
+            $table->string('url')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_items');
+    }
+};
